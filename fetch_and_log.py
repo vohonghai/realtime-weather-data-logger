da8e7d1ed565cd2a250361155e49e40525013a0a
vohonghai <vhhai@hcmus.edu.vn>
2025-05-28 15:56:38 +0700

    Update fetch_and_log.py

--- fetch_and_log.py
+++ fetch_and_log.py
@@ -1,19 +1,15 @@
-# fetch_and_log.py
 import requests, json, os
 from datetime import datetime
 
 url = "https://realtime-weather-api-1.onrender.com/data"
 filename = "data/history.json"
 
-# Fetch data
 r = requests.get(url)
 data = r.json()
 data['fetched_at'] = datetime.now().isoformat()
 
-# Tạo folder nếu chưa có
 os.makedirs("data", exist_ok=True)
 
-# Đọc dữ liệu cũ
 if os.path.exists(filename):
     with open(filename, 'r') as f:
         try:
@@ -23,9 +19,7 @@ if os.path.exists(filename):
 else:
     history = []
 
-# Thêm dữ liệu mới
 history.append(data)
 
-# Ghi lại
 with open(filename, 'w') as f:
     json.dump(history, f, indent=2)
